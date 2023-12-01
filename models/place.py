86ea3cd0342355da9b7cb533b9dfb439b7b58b91
lucieruin <lucie.wattez@hotmail.fr>
2023-12-01 13:29:20 +0100

    update place.py & amenity.py & add a relationship between place and amenity

--- models/place.py
+++ models/place.py
@@ -3,13 +3,47 @@
 import models
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.amenity import Amenity
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenity = Table(
+                'place_amenity',
+                Base.metadata,
+                Column(
+                    'place_id',
+                    String(60),
+                    ForeignKey('places.id'),
+                    primary_key=True,
+                    nullable=False
+                ),
+                Column(
+                    'amenity_id',
+                    String(60),
+                    ForeignKey('amenities.id'),
+                    primary_key=True,
+                    nullable=False
+                )
+            )
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -26,6 +60,8 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
                                cascade="all, delete", passive_deletes=True)
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -35,3 +71,19 @@ class Place(BaseModel, Base):
                 if value.place_id == self.id:
                     list_reviews.append(value)
             return list_reviews
+
+        @property
+        def amenities(self):
+            """ amenities getter"""
+            amenities_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, cls):
+            """ amenities setter """
+            if not isinstance(cls, Amenity):
+                return
+            self.amenity_ids.append(cls.id)
